30a988d85287123244f19cbcc13dd065fa980cda
Reza <i.am.reza.programmer@gmail.com>
2024-09-02 21:19:31 +0330

    opening system

---
+++ src/assets/js/app.js
@@ -0,0 +1,12 @@
+const btnPnlsLst = document.querySelectorAll(".panel-btn");
+const pnlPageLst = document.querySelectorAll(".panel-page")
+
+btnPnlsLst.forEach(btn => {
+    btn.addEventListener("click" , function (){
+        const _type = this.dataset.type;
+        const activePnl = document.querySelector('.panel-page--active');
+        const targetPage = Array.from(pnlPageLst).find(pnl => pnl.dataset.type === _type)
+        targetPage.classList.toggle("panel-page--active")
+        if(activePnl && activePnl !== targetPage) activePnl.classList.remove('panel-page--active')
+    })
+})
