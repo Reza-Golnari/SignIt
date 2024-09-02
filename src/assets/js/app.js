fa1e7375396ad725cc8aedd12dafda2b44faaa5b
Reza <i.am.reza.programmer@gmail.com>
2024-09-02 23:08:57 +0330

    bug

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -19,7 +19,7 @@ window.addEventListener("DOMContentLoaded" , ()=> {
     const sizeInput = document.querySelector("#size");
     const radiusInput = document.querySelector("#radius");
 
-    let color = "#ffffff";
+    let color = "#000000";
     let size = 4;
     let radius = 4;
 
@@ -30,4 +30,32 @@ window.addEventListener("DOMContentLoaded" , ()=> {
     colorInput.addEventListener("input" , e => color = e.target.value);
     sizeInput.addEventListener("input" , e => size = e.target.value);
     radiusInput.addEventListener("input" , e => radius = e.target.value);
+
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+    let isDrawing = false;
+
+    canvas.width = window.innerWidth / 3;
+    canvas.height = window.innerHeight / 2;
+
+    canvas.addEventListener('mouseup' , () => {
+        isDrawing = false;
+        ctx.beginPath();
+    })
+    canvas.addEventListener('mousedown' , () => isDrawing = true)
+    canvas.addEventListener("mousemove" , e => {
+        if(!isDrawing) return;
+
+        ctx.lineWidth = 2;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = '#000000';
+
+        console.log(e.clientX , e.clientY)
+        console.log(canvas.offsetLeft , canvas.offsetTop)
+
+        ctx.lineTo(e.offsetX - canvas.offsetLeft , e.offsetY - canvas.offsetTop);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.offsetX - canvas.offsetLeft , e.offsetY - canvas.offsetTop);
+    })
 })
